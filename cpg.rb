30ddd08c7dbc6f123550e3e12f7db326143788fe
RyotaBannai <ryotala0528@gmail.com>
2022-01-16 03:15:53 +0900

    chore: updated 0.0.2-beta

--- cpg.rb
+++ cpg.rb
@@ -1,8 +1,8 @@
 class Cpg < Formula
   desc "Competitive Programming Grader for automating coding-build-testing loop."
   homepage "https://github.com/RyotaBannai/competitive-programming-grader"
-  url "https://github.com/RyotaBannai/competitive-programming-grader/releases/download/0.0.1-release/cpg"
-  sha256 "3b1d6adbf87c3c1d18d730772c6599c8f2c181370a79618da2a2a7b1cfbaba29"
+  url "https://github.com/RyotaBannai/competitive-programming-grader/releases/download/0.0.2-beta/cpg"
+  sha256 "1f28aced703f0da40487c743e86d26a7120d994c942d02a2a89fca0af91ce9d5"
   license "MIT"
 
   def install
